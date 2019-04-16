c17043c233373fadcf88f6049b3dc9c777459773
Teemu Litmanen <40266954+Teemuli@users.noreply.github.com>
2019-04-16 16:17:39 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const pgp = require('pg-promise')();
 const API_PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
-const router = express.Router();
+//const router = express.Router();
 
 
 const dbRoute = 'postgres://bfacpgrwovkgzr:6a8e9b5b8c62098fdd4e537f3246b8b7b402ccb2aa324eaaddf6809e41a66d2b@ec2-79-125-2-142.eu-west-1.compute.amazonaws.com:5432/d88cb0454fotvb?ssl=1';
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger("dev"));
 
-router.get("/getData", (req, res) => {
+app.get("api/getData", (req, res) => {
   db.any('SELECT * FROM test')
         .then(function (data) {
             console.log('DATA:', data)
@@ -37,7 +37,7 @@ router.get("/getData", (req, res) => {
 
 // this is our create methid
 // this method adds new data in our database
-router.post("/putData", (req, res) => {
+app.post("api/putData", (req, res) => {
   const { test_id, user_id, exposure_num } = req.body;
   db.none('INSERT INTO test(test_id, user_id, exposure_num) VALUES($1, $2, $3)'
     ,[test_id, user_id, exposure_num]);
@@ -53,7 +53,7 @@ router.post("/putData", (req, res) => {
 });
 
 // append /api for our http requests
-app.use("/api", router);
+//app.use("/api", router);
 
 // launch our backend into a port
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
