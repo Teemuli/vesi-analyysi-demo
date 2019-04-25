c7dd305aa6902a4b0dc2e37ce22181a1ce32c2b8
Teemu Litmanen <40266954+Teemuli@users.noreply.github.com>
2019-04-25 16:16:19 +0300

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 
 // /client/App.js
 import React, { Component } from "react";
+import './App.css';
 import axios from "axios";
+import MapContainer from './components/MapContainer';
+import UploadComponent from './components/UploadComponent';
 
 class App extends Component {
   // initialize our state 
   state = {
-    data: [],
-    test_id: null,
-    user_id: null,
-    expsure_num: null
+    data: []
   };
 
   // when component mounts, first thing it does is fetch all existing data in our db
@@ -23,11 +23,6 @@ class App extends Component {
   // always kill a process everytime we are done using it
   componentWillUnmount() {
   }
-
-  // just a note, here, in the front end, we use the id key of our data object 
-  // in order to identify which we want to Update or delete.
-  // for our back end, we use the object id assigned by MongoDB to modify 
-  // data base entries
 
   // our first get method that uses our backend api to 
   // fetch data from our data base
@@ -48,42 +43,6 @@ class App extends Component {
       exposure_num: Math.random()*100
     });
   };
-  
- /*
-  // our delete method that uses our backend api 
-  // to remove existing database information
-  deleteFromDB = idTodelete => {
-    let objIdToDelete = null;
-    this.state.data.forEach(dat => {
-      if (dat.id == idTodelete) {
-        objIdToDelete = dat._id;
-      }
-    });
-
-    axios.delete("http://localhost:3001/api/deleteData", {
-      data: {
-        id: objIdToDelete
-      }
-    });
-  };
-
-
-  // our update method that uses our backend api
-  // to overwrite existing data base information
-  updateDB = (idToUpdate, updateToApply) => {
-    let objIdToUpdate = null;
-    this.state.data.forEach(dat => {
-      if (dat.id == idToUpdate) {
-        objIdToUpdate = dat._id;
-      }
-    });
-
-    axios.post("http://localhost:3001/api/updateData", {
-      id: objIdToUpdate,
-      update: { message: updateToApply }
-    });
-  };
-  */
 
   // here is our UI
   // it is easy to understand their functions when you 
@@ -92,16 +51,19 @@ class App extends Component {
     const { data } = this.state;
     return (
       <div>
-        <ul>
-          {data.map(dat => (<div>{dat.test_id}</div>))}
-          <button
-            onClick={() =>
-              this.putDataToDB()
-            }
-          >
-            insert data
-          </button>
-        </ul>
+        <div className="App">
+            <header className="App-header">
+              <script>
+              </script>
+              <h3>
+                UEF sovelletun fysiikan laitoksen vesianalyysi WebApp
+              </h3>
+            </header>
+            <div className="container">
+            <div><UploadComponent /></div>
+          </div>
+          </div>
+        <MapContainer/>
       </div>
       
     );
