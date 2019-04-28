e96ec5a42753ac05f371b3821b7d4304dcfacf82
Teemu Litmanen <40266954+Teemuli@users.noreply.github.com>
2019-04-28 16:28:34 +0300

    add graphcomponent

---
+++ client/src/components/GraphComponent.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+
+export class GraphComponent extends Component{
+
+    state = {
+        data: [],
+    };
+
+    componentDidMount() {
+        this.getDataFromDb();
+    }
+    
+    getDataFromDb = () => {
+        fetch("api/getData")
+          .then(data => data.json())
+          .then(res => this.setState({ data: res.data }));
+    };
+
+    render() {
+        const { data } = this.state;
+        return (
+            <div>
+                <LineChart width={600} height={600} data={data} position={"right"}>
+                    <Line type="monotone" dataKey="result_manganese" stroke="#8884d8" />
+                    <CartesianGrid stroke="#ccc" />
+                    <XAxis dataKey="test_id" />
+                    <YAxis />
+                </LineChart>
+            </div>
+        );
+    }
+}
+export default GraphComponent;
