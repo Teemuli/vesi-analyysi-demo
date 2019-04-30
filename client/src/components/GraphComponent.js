fc534fe5a14d5ff28591908348b60f6fcd80d73e
Teemu Litmanen <40266954+Teemuli@users.noreply.github.com>
2019-04-30 16:46:27 +0300

    update graphcomponent

--- client/src/components/GraphComponent.js
+++ client/src/components/GraphComponent.js
@@ -19,9 +19,14 @@ export class GraphComponent extends Component{
 
     render() {
         const { data } = this.state;
+        var databyid = [];
+        data.map(dat => {
+            if(dat.test_id === this.props.currentId)
+            databyid.push(dat);
+        })
         return (
             <div>
-                <LineChart width={600} height={600} data={data} position={"right"}>
+                <LineChart width={600} height={600} data={databyid} position={"right"}>
                     <Line type="monotone" dataKey="result_manganese" stroke="#8884d8" />
                     <CartesianGrid stroke="#ccc" />
                     <XAxis dataKey="test_id" />
