4e11a5d550920b35e96c6033289f95d479e99e46
tomironkko <tomi.ronkko@gmail.com>
2019-05-01 17:55:29 +0300

    Remove unused code

--- client/src/components/UploadComponent.js
+++ client/src/components/UploadComponent.js
@@ -6,24 +6,6 @@ import DropzoneComponent from './DropzoneComponent';
 
 export class UploadComponent extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.fileInput = React.createRef();
-    }
-    
-    handleSubmit(event) {
-        event.preventDefault();
-        alert(
-        `
-        Tiedosto tallennettu:
-
-        ${this.fileInput.current.files[0].name}
-        `
-        );
-
-        }
-
     render() {
         return (
 
